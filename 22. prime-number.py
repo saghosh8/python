b0bacdd2dc5f6c23d5053e8afb042c38baef70a3
saghosh <sahanaghosh8@gmail.com>
2024-09-11 08:33:01 +0530

    adding files

--- 22. prime-number.py
+++ 22. prime-number.py
@@ -1,21 +1,14 @@
 # check if the Number is prime
 
 # Input number
-num = int(input("Enter a number: "))
+N = int(input("Enter a number: "))
 
-# Step 1: Calculate factorial of the number manually
-fact = 1
-for i in range(1, num + 1):
-    fact *= i
+fact = []
+for i in range(1, N+1):
+    if N % i == 0:
+        fact.append(i)
 
-# Step 2: Create a list of factors of the factorial
-factors = []
-for i in range(1, num + 1):
-    if fact % i == 0:
-        factors.append(i)
-
-# Step 3: Check if the number is prime
-if factors == [1, num]:
-    print(f"Yes, {num} is a prime number!")
+if len(fact) == 2:
+    print('This is a Prime Number')
 else:
-    print(f"Sorry, {num} is not a prime number.")
+    print('This is NOT a Prime Number')
