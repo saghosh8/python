26d2cf83cbd12dd5f9c9a084f86368d784ca4875
saghosh <sahanaghosh8@gmail.com>
2024-09-20 21:07:06 +0530

    adding files

--- array/4. remove_max_freq_element.py
+++ array/4. remove_max_freq_element.py
@@ -3,28 +3,16 @@
 #    - WithOut pop
 
 class Solution:
-    def find_freq(self, nums):
+   
+    def removeMaxFreq(self, nums):
         frequency = {}
-        
-        # Count frequency of each element
-        for num in nums:
-            frequency[num] = frequency.get(num, 0) + 1
+        for i in nums:
+            frequency[i] = frequency.get(i, 0) + 1
+        max_freq_element = max(frequency, key=frequency.get)
+        del frequency[max_freq_element]
         return frequency
 
-    def remove_max_freq_element(self, nums):
-        frequency = self.find_freq(nums)
-        
-        # Get the maximum frequency
-        max_freq = max(frequency.values())
-        
-        # Find the elements with maximum frequency
-        max_freq_elements = [num for num, freq in frequency.items() if freq == max_freq]
 
-        # Create a new list excluding elements with maximum frequency
-        nums = [num for num in nums if num not in max_freq_elements]
-        
-        return nums
-
-if __name__ == '__main__':          
-    nums = [1, 2, 3, 4, 5, 6, 1, 2, 2]
-    print(Solution().remove_max_freq_element(nums))
+if __name__ == '__main__':
+    nums = [1, 2, 2, 3, 2, 4, 2]
+    print(Solution().removeMaxFreq(nums))
