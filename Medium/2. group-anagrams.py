45c6d2fef37fb256825c6812f732dcb6f7aa0c90
saghosh <sahanaghosh8@gmail.com>
2024-09-24 10:04:33 +0530

    adding files

---
+++ Medium/2. group-anagrams.py
@@ -0,0 +1,7 @@
+# Group Anagrams
+# Given an array of strings, group anagrams together.
+
+# Example Input: ["eat", "tea", "tan", "ate", "nat", "bat"]
+# Example Output: [['eat', 'tea', 'ate'], ['tan', 'nat'], ['bat']]
+
+
