4179538084b22ada8e6e9e38f8c9155b5b13696f
saghosh <sahanaghosh8@gmail.com>
2024-11-20 20:10:25 +0530

    added

--- Notes/3. bubbleSort.py
+++ Notes/3. bubbleSort.py
@@ -1,15 +1,22 @@
 def bubbleSort(array):
     n = len(array)
+    # Outer loop to traverse through all elements
     for i in range(n):
+        # Track if any swaps happen in this iteration
         swapped = False
-        for j in range(0, n-i-1):
-            if (array[j] >= array[j+1]):
+        # Inner loop for comparison within the unsorted part of the array
+        for j in range(n-1-i):
+            # Compare adjacent elements
+            if array[j] > array[j+1]:  
+                # Swap if elements are in the wrong order
                 array[j], array[j+1] = array[j+1], array[j]
-                swapped = True
-        if not swapped:
+                # Mark that a swap has occurred
+                swapped = True  
+        # If no swaps, array is sorted       
+        if not swapped:  
             break
-    return array
+    # Return the sorted array   
+    return array  # Return the sorted array
 
-
-array = [64, 34, 25, 12, 22, 11, 90]
+array = [60, 50, 95, 80, 70]
 print(bubbleSort(array))
